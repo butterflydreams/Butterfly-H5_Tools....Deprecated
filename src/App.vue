3bd5791f2371e3ec65cad3a70dd66f8de0bde5ec
butterflydreams <49908618+butterflydreams@users.noreply.github.com>
2022-02-15 19:50:25 +0800

    add: files.

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<style scoped lang="less"></style>
+
+<template>
+  <router-view></router-view>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {};
+  },
+  created() {
+    document.title = "Compilation Tools";
+  },
+  methods: {}
+};
+</script>
