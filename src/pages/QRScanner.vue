46bdf9b01fc479efc49e404e8b2d4e9739f0012d
butterflydreams <inbutterflydream@gmail.com>
2022-02-16 11:53:20 +0800

    add: local reader.

--- src/pages/QRScanner.vue
+++ src/pages/QRScanner.vue
@@ -31,6 +31,43 @@
     }
   }
   .ui-localreader {
+    position: relative;
+    margin: 0 auto;
+    width: 100%;
+    height: 100vh;
+    background-color: rgba(0, 0, 0, 0.2);
+    .wrapper {
+      overflow: hidden;
+      position: absolute;
+      margin: auto;
+      top: 42%;
+      left: 0;
+      right: 0;
+      transform: translateY(-50%);
+      width: 5rem;
+      height: 5.4rem;
+      border-radius: 0.24rem;
+      background-color: #ffffff;
+      .actions {
+        display: flex;
+        justify-content: left;
+        align-items: center;
+        position: relative;
+        margin: 0 auto;
+        padding-left: 0.14rem;
+        width: 100%;
+        height: 0.6rem;
+        border-bottom: solid 0.02rem #d6d6d6;
+        .action {
+          position: relative;
+          margin: 0 0.06rem;
+          width: 0.26rem;
+          height: 0.26rem;
+          border-radius: 50%;
+          background-color: #000000;
+        }
+      }
+    }
   }
   .ui-camerascan {
   }
@@ -42,7 +79,15 @@
     <div class="ui-serverdecode" v-if="type == ETYPE.eServerDecode">
       <div class="txt-tips">Sorry, your device is unsupport yet.</div>
     </div>
-    <div class="ui-localreader" v-else-if="type == ETYPE.eLocalReader"></div>
+    <div class="ui-localreader" v-else-if="type == ETYPE.eLocalReader">
+      <div class="wrapper">
+        <a class="actions" href="javascript:;" @click="OnCloseClick()">
+          <div class="action" style="background-color: #ef1910"></div>
+          <div class="action" style="background-color: #ffc54a"></div>
+          <div class="action" style="background-color: #00a584"></div>
+        </a>
+      </div>
+    </div>
     <div class="ui-camerascan" v-else></div>
   </section>
 </template>
