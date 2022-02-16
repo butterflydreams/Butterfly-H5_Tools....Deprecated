7d28b65bec941292337c64b9c8376cae2ba5404d
butterflydreams <inbutterflydream@gmail.com>
2022-02-16 11:27:09 +0800

    remove: Upload component.

--- src/pages/QRScanner.vue
+++ src/pages/QRScanner.vue
@@ -13,9 +13,7 @@
 </style>
 
 <template>
-  <section class="qrscanner">
-    <ui-upload tips="<span style='text-decoration: underline'>Click here</span> to decode your image QRCode" @OnCallback="OnUploadCallback" @OnClose="OnUploadClose"></ui-upload>
-  </section>
+  <section class="qrscanner"></section>
 </template>
 
 <script>
@@ -24,9 +22,7 @@ const ETYPE = {
   eLocalReader: 1, //Decode local image : iOS | iPadOS < 14.0 or all android.
   eServerDecode: 2 //Upload image to sever decode: old device or system, unsupport yet.
 };
-import UIUpload from "@ui/Upload.vue";
 import { Versions } from "@lib/util/compare.js";
-import { BrowserQRCodeReader } from "@zxing/browser";
 export default {
   name: "QRScanner",
   data() {
@@ -38,55 +34,6 @@ export default {
     document.title = "QR Code Scanner";
     this.type = Versions.Compare(this.iOSVersion, "14.0", ">=") == true || Versions.Compare(this.iPadOSVersion, "14.0", ">=") == true ? ETYPE.eCameraScan : ETYPE.eLocalReader;
   },
-  components: {
-    "ui-upload": UIUpload
-  },
-  methods: {
-    OnUploadCallback(_, reuslt) {
-      if (!!reuslt) {
-        let scanner = new BrowserQRCodeReader();
-        if (!!scanner) {
-          scanner
-            .decodeFromImageUrl(reuslt)
-            .then((data) => {
-              this.$alert.present({
-                title: "Success",
-                message: data.text,
-                actions: [
-                  {
-                    name: "Cancel",
-                    callback: (id) => {
-                      this.$alert.dismiss(id);
-                    }
-                  },
-                  {
-                    name: "To Website",
-                    style: "font-weight: 500; color: #ff0000",
-                    callback: (id) => {
-                      this.$alert.dismiss(id);
-                      window.location.href = data.text;
-                    }
-                  }
-                ]
-              });
-            })
-            .catch(() => {
-              this.$alert.present({
-                title: "<span style='color: #ff0000'>Fail</span>",
-                message: "Image decode error!",
-                actions: [
-                  {
-                    name: "OK"
-                  }
-                ]
-              });
-            });
-        }
-      }
-    },
-    OnUploadClose() {
-      this.$router.go(-1);
-    }
-  }
+  methods: {}
 };
 </script>
